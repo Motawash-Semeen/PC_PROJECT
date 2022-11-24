568f92c7d8af38918285ff8c1d035736afc396be
Harmfullllll <tanvirjoy114@gmail.com>
2022-11-24 23:25:50 +0600

    change

--- app/src/main/java/com/jovanovic/stefan/sqlitetutorial/Register.java
+++ app/src/main/java/com/jovanovic/stefan/sqlitetutorial/Register.java
@@ -1,14 +1,73 @@
 package com.jovanovic.stefan.sqlitetutorial;
 
+import static android.text.TextUtils.isEmpty;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
+
+    EditText fname;
+    EditText lname;
+    EditText uname;
+    EditText em;
+    EditText pass;
+    Button b1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        fname = findViewById(R.id.f1_name);
+        lname = findViewById(R.id.l1_name);
+        uname = findViewById(R.id.u1_name);
+        em = findViewById(R.id.e1_name);
+        pass = findViewById(R.id.p1_name);
+        b1 = findViewById(R.id.buu1);
+
+       b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkthedata();
+            }
+        });
+    }
+
+    boolean isEmpty(EditText text) {
+        CharSequence string = text.getText().toString();
+        return TextUtils.isEmpty(string);
+    }
+
+    void checkthedata() {
+        if (isEmpty(fname)) {
+            Toast t = Toast.makeText(this, "You must enter first name to register!", Toast.LENGTH_SHORT);
+            t.show();
+        }
+
+        if (isEmpty(lname)) {
+            Toast t = Toast.makeText(this, "You must enter last name to register!", Toast.LENGTH_SHORT);
+            t.show();
+        }
+
+        if (isEmpty(uname)) {
+            uname.setError("You must enter username to register!");
+        }
+
+        if (isEmpty(pass)) {
+            pass.setError("You must enter password to register!");
+        }
+        if (isEmpty(em)) {
+            em.setError("You must enter email to register!");
+        }
+
     }
 }
