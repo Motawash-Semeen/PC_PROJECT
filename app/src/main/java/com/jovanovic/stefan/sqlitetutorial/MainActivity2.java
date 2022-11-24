d6bceda4372ecc8f6f89696abaf73aafd0dfd8d4
MOtawash Semeen Chowdhury <motawash15-14166@diu.edu.bd>
2022-11-24 19:36:20 +0600

    1ST

---
+++ app/src/main/java/com/jovanovic/stefan/sqlitetutorial/MainActivity2.java
@@ -0,0 +1,27 @@
+package com.jovanovic.stefan.sqlitetutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private static int TIME_OUT = 4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIME_OUT);
+    }
+}
