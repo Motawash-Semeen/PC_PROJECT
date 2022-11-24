c3757e47422c7b09436c74cdfab55209087049bc
MOtawash Semeen Chowdhury <motawash15-14166@diu.edu.bd>
2022-11-24 21:18:53 +0600

    2nd

---
+++ app/src/main/java/com/jovanovic/stefan/sqlitetutorial/Home.java
@@ -0,0 +1,26 @@
+package com.jovanovic.stefan.sqlitetutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Home extends AppCompatActivity {
+
+    private static int TIME_OUT = 4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Home.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIME_OUT);
+    }
+}
